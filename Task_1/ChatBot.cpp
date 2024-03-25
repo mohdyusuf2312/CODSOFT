18a3f7677723fd1f91cd721beb6f075cbbdac0dd
Muhammad Yusuf <mohdyusufr@gmail.com>
2024-03-25 20:15:17 +0530

    Add get Response

--- Task_1/ChatBot.cpp
+++ Task_1/ChatBot.cpp
@@ -1,7 +1,20 @@
-#include <iostream>
-#include <string>
-
+#include<iostream>
+#include<string>
+#include<algorithm>
 using namespace std;
+
+string getResponse(const string& userInput) {
+    // Pattern matching and response selection
+    if (userInput == "hello" || userInput == "hey" || userInput == "hi" || userInput == "yup" || userInput == "listen") {
+        return "Hello! How can I assist you?";
+    } else if (userInput == "how are you\\?" || userInput == "how do you do\\?") {
+        return "I'm just a chatbot, but thanks for asking!";
+    } else if (userInput == "bye" || userInput == "go away" || userInput == "goodbye" || userInput == "good bye") {
+        return "Goodbye! Have a great day!";
+    } else {
+        return "I'm sorry, I didn't understand that.";
+    }
+}
 
 int main() {
     cout << "Welcome to the ChatBot!\n";
@@ -11,13 +24,14 @@ int main() {
     while (true) {
         cout << "You: ";
         getline(cin, userInput);
-        
+        transform(userInput.begin(), userInput.end(), userInput.begin(), ::tolower); // Convert input to lowercase
         if (userInput == "bye" || userInput == "Goodbye" || userInput == "see you") {
             cout << "Chatbot: Goodbye! Have a great day!\n";
             break;
         }
         
-        
+        string response = getResponse(userInput);
+        cout << "Chatbot: " << response << endl;
     }
     
     return 0;
