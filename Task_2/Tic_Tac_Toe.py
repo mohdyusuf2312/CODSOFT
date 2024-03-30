31c202a90f0a4f49a7c04b9fc8ec7834b19e0352
Muhammad Yusuf <mohdyusufr@gmail.com>
2024-03-30 15:54:08 +0530

    Update print_board method and player_move method

--- Task_2/Tic_Tac_Toe.py
+++ Task_2/Tic_Tac_Toe.py
@@ -5,7 +5,7 @@ print("The board's index values are: ")
 print("0|1|2")
 print("3|4|5")
 print("6|7|8")
-print("Enter your move, 0 through 8. (You are 0's and AI is X's) ")
+print("Enter your move, 0 through 8. (You are X's and AI is O's) ")
 
 board = ["_","_","_","_","_","_"," "," "," "] #Board horizontal lines
 pairs = ([0,3,6],[1,4,7],[2,4,8],[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6]) #Win Pairs
@@ -20,13 +20,23 @@ def print_board(turn, board, aiturn):
     print(board[3] + "|" + board[4] + "|" + board[5])
     print(board[6] + "|" + board[7] + "|" + board[8])
     print("Turn :" + str(turn))
-
+    if turn == 0: #For first move
+        player_move(turn, board, aiturn)
+    if turn == "AI": #if ai_move
+        aiturn += 1
+        ai_move(turn, board, aiturn, corner)
+    if turn == "PLAYER": #if player_move
+        player_move(turn, board, aiturn)
 
 def player_move(turn, board, aiturn):
     choice = input("Enter your move (0-8): ")
+    board[int(choice)] = "X" # update board when player's move have done.
+    turn = "AI" # change turn to AI, now its ai_move
+
 
 
 def ai_move(turn, board, aiturn, corner):
     data = []
+
 
 print_board(turn, board, aiturn)
